e2c9647a2ccffd5653401b6e8456897e865c7b76
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-20 01:04:37 -0400

    Update Find a string.py

--- python/Find a string.py
+++ python/Find a string.py
@@ -1,16 +1,25 @@
 https://www.hackerrank.com/challenges/find-a-string/problem?isFullScreen=true 
 
-  def count_substring(string, sub_string):
+def count_substring(string, sub_string):
   count = 0
-  for i in range(0, len(string)):
-    for j in range(0, len(sub_string)):
-      if string[i] == sub_string[j]:
-        i +=1
-        j +=1
-      else:
-        i+=1
-        
-    count +=1   
-    return count
+  str_len = len(string)
+  sub_len = len(sub_string)
+  for i in range(str_len - sub_len + 1 ):
+    if(string[i:i+sub_len] == sub_string) : # using string slicing
+      
+    # for j in range(len(sub_string)):
+    #   if sub_string[j] ==string[i]:
+    #     i +=1
+    #     j +=1 
+    #   else:
+    #     i+=1
+      count +=1   
+  return count
+
 
 if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
