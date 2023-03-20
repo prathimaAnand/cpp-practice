82376b7c680912386a84c10eb3d54d050fd98cff
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-20 01:36:09 -0400

    Update classes and objects.cpp

--- C++ codes/classes and objects.cpp
+++ C++ codes/classes and objects.cpp
@@ -1,1 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
 
+// Write your Student class here
+class Student{
+  public:
+  int s1,s2,s3,s4,s5;
+    void input(){
+      
+      cin >> s1 ;
+      cin >> s2 ;
+      cin >> s3 ;
+      cin >> s4 ;
+      cin >> s5 ;
+    }
+    int calculateTotalScore(){
+      return s1+s2+s3+s4+s5;
+    }
+};
+
+int main() {
