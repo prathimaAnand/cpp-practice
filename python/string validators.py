dc5c077ac1d8a5d0fac7fc0ebd1fea70501a6b04
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-21 18:00:11 -0400

    Update string validators.py

--- python/string validators.py
+++ python/string validators.py
@@ -1,1 +1,28 @@
 
+# only 3 test cases passed
+# yet to solve
+if __name__ == '__main__':
+    s = input()
+    for i in range(len(s)):
+      if s[i].isalnum():
+        print("True")
+      else: 
+        print("False")
+      if s[i].isalpha():
+        print("True")
+      else: 
+        print("False")
+      if s[i].isdigit():
+        print("True")
+      else: 
+        print("False")
+      if s[i].islower():
+        print("True")
+      else: 
+        print("False")
+      if s[i].isupper():
+        print("True")
+      else: 
+        print("False") 
+  
+      
