1303338703a39f88a33f06c10329bb3e282f1df5
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-08-05 11:11:06 -0400

    Update intro to functions.c

--- C practise/intro to functions.c
+++ C practise/intro to functions.c
@@ -20,3 +20,25 @@ int main() {
     printf("%d", greatest_of_four);
     return 0;
 }
+
+//Optimized solution
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d){
+    int max = 0;
+    //use ternary operator to reduce code lines
+    max=((a>b)&&(a>c)&&(a>d))?a\
+            :(((b>a)&&(b>c)&&(b>d))?b\
+            :(((c>a)&&(c>b)&&(c>d))?c\
+            :d));
+    
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int greatest_of_four = max_of_four(a, b, c, d);
+    printf("%d", greatest_of_four);
+    return 0;
+}
