aad282b0c3bd98e6ab714a0d88e6f0aa54c7a715
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2024-01-17 11:53:24 -0500

    Update vector sort.cpp

--- C++ codes/vector sort.cpp
+++ C++ codes/vector sort.cpp
@@ -4,24 +4,47 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-
+ 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    // delcaring and reading inputs
-    vector<int> v;
-    int n;
-    cin>>n;
-    for (int i; i<n; i++){
-        int element;
-        cin>>element;
-        v.push_back(element);
+    // store all the numbers
+    int num_i, query, no_queries;
+    vector<int> v(num_i);
+    for (int i = 0; i < v.size(); i++){
+        // int ele;
+        // cin>>ele;
+        // v.push_back(ele);
+        cin>>v[i];
     }
-    // sorting and writing sorted vector to output
     sort(v.begin(),v.end());
-    for(int j = 0; j < n; j++){
-        cout<<v.at(j)<<" ";// element with space
+    for(int l =0; l<v.size();l++){
+        cout<<"v printing";
+        cout<<v[l];
     }
+    // how to receive diff queries
+    // for(int j = 0; j >= no_queries; j++){
+        
+    // }
+    // for(int k = 0; k < v.size(); k++){
+    //     if(query == v.at(k))
+    //         cout<<"Yes "<<k;
+    //     else
+    //         if(query > v.at(k))
+    //         cout<<"No "<<k;
+    // }
+    // cout<<"outside while";
+    cin>>query;
+    while(query--){// false when query = 0
+        // cout<<"inside while";
+        int y;
+        cin>>y;
+        auto z = lower_bound(v.begin(),v.end(), y);
+        if(z != v.end() && y == *z)
+            cout<<"Yes "<<z - v.begin()<<endl;
+        else
+            cout<<"No "<<z - v.begin()<<endl;
+    }  
     return 0;
+    
 }
 
